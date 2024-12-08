c011e470577490b63b9877fc133e2f102e296386
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2024-12-08 10:38:12 +0500

    Upload initial files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const hamburger = document.getElementById("hamburger");
+const list = document.getElementById("list");
+const overlay = document.getElementById("overlay");
+const icon = document.getElementById("icon");
+
+hamburger.addEventListener("click", (e) => {
+    e.preventDefault();
+    list.classList.toggle("nav-list-active");
+    overlay.classList.toggle("overlay-active");
+    icon.classList.toggle("fa-xmark");
+});
+
+overlay.addEventListener("click", () => {
+    list.classList.remove("nav-list-active");
+    overlay.classList.remove("overlay-active");
+});
